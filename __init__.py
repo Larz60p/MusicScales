bd1c394a1a947c17b53a14a1d58f5ae624fd6add
Larz60+ <Larz60p@users.noreply.github.com>
2017-03-07 16:54:09 -0500

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,43 @@
+#
+# MIT License
+#
+# Copyright (c) 2017 Larz60+
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+#    A Huge amount of Credit goes to  Francesco Balena, http://www.saxopedia.com
+#    for his tremendous work on the publication 'The Scale Omnibus' which was my major
+#    source for Interval patterns of scales from around the world.
+#
+#    All scores were created using lilypond -LilyPond is a music engraving program,
+#    devoted to producing the highest-quality sheet music possible. It brings the
+#    aesthetics of traditionally engraved music to computer printouts. LilyPond is
+#    free software and part of the GNU Project.
+#    Available at: http://www.lilypond.org/
+#
+#    If you have new material, including scales which you have created, please send
+#    that information to me and to Francesco, along with a publication release, and
+#    I will try and add it to the next software release
+#
+__init__.py
+McNote.py
+utilities.py
+sdata.py
+chords.py
+scales.py
